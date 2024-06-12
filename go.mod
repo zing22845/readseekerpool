6d83290939b3772693c3bf837b500e0c4cc695e2
zing22845 <60884519+zing22845@users.noreply.github.com>
2024-06-12 10:36:58 +0800

    init repo

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/zing22845/readseekerpool
+
+go 1.21.5
+
+require (
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.55.1
+	github.com/zing22845/s3readseeker v0.0.0-20240607102045-5c7c5a405778
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.27.2 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.9 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
+)
---
+++ readseekerpool.go
@@ -0,0 +1,100 @@
+package readseekerpool
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type ReadSeekerPool struct {
+	pool      *sync.Pool
+	rsType    string
+	params    []interface{}
+	initNew   func() (io.ReadSeeker, error)
+	poolSize  int
+	currCount chan struct{}
+}
+
+func NewReadSeekerPool(rsType string, poolSize int, params ...interface{}) (rsp *ReadSeekerPool, err error) {
+	pool := &ReadSeekerPool{
+		rsType:    rsType,
+		params:    params,
+		poolSize:  poolSize,
+		currCount: make(chan struct{}, poolSize),
+	}
+
+	switch rsType {
+	case "s3":
+		if len(params) != 3 {
+			return nil, fmt.Errorf("init s3 read seeker requires client, bucket and keyGroup")
+		}
+		client, ok := params[0].(*s3.Client)
+		if !ok {
+			return nil, fmt.Errorf("invalid client param for s3 read seeker")
+		}
+		bucket, ok := params[1].(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid bucket param for s3 read seeker")
+		}
+		keyGroup, ok := params[2].([]string)
+		if !ok {
+			return nil, fmt.Errorf("invalid keyGroup param for s3 read seeker")
+		}
+		pool.initNew = func() (io.ReadSeeker, error) {
+			s3RS, err := NewS3ReadSeeker(client, bucket, keyGroup)
+			if err != nil {
+				return nil, err
+			}
+			return s3RS, nil
+		}
+	case "file":
+		if len(params) != 1 {
+			return nil, fmt.Errorf("init file read seeker requires file path")
+		}
+		filePath, ok := params[0].(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid filePath param for file read seeker")
+		}
+		pool.initNew = func() (io.ReadSeeker, error) {
+			f, err := os.Open(filePath)
+			if err != nil {
+				return nil, err
+			}
+			return f, nil
+		}
+	default:
+		return nil, fmt.Errorf("unsupported read seeker type %s", rsType)
+	}
+	// Initialize pool
+	pool.pool = &sync.Pool{
+		New: func() interface{} {
+			rs, err := pool.initNew()
+			if err != nil {
+				return nil
+			}
+			return rs
+		},
+	}
+	return pool, nil
+}
+
+func (p *ReadSeekerPool) Get() (io.ReadSeeker, error) {
+	p.currCount <- struct{}{}
+	v := p.pool.Get()
+	if v == nil {
+		return nil, fmt.Errorf("pool is empty")
+	}
+	rs, ok := v.(io.ReadSeeker)
+	if !ok {
+		return nil, fmt.Errorf("pooled object does not implement io.ReadSeeker")
+	}
+	return rs, nil
+}
+
+func (p *ReadSeekerPool) Put(rs io.ReadSeeker) {
+	p.pool.Put(rs)
+	<-p.currCount
+}
